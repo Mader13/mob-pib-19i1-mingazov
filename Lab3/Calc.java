9e85e75b1c4b0641252bbcc69d039e12e3a6e4d0
Mader13 <59108331+Mader13@users.noreply.github.com>
2021-11-06 18:11:56 +0600

    Update Calc.java

--- Lab3/Calc.java
+++ Lab3/Calc.java
@@ -2,47 +2,38 @@ import java.util.Scanner;
 
 public class Calc {
 
-    public static void main(String[] args){
-        double num1, num2, result;
-
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String example = input.nextLine();
         String[] digitsStr = example.split(" ");
-        num1 = Double.parseDouble(digitsStr[0]);
-        num2 = Double.parseDouble(digitsStr[2]);
-
-        switch (digitsStr[1]) {
-            case ("+") :
-                result = num1 + num2;
-                correctnessOfResult(result);
-                break;
-            case ("-") :
-                result = num1 - num2;
-                correctnessOfResult(result);
-                break;
-            case ("/") :
-                result = num1 / num2;
-                correctnessOfResult(result);
-                break;
-            case ("*") :
-                result = num1 * num2;
-                correctnessOfResult(result);
-                break;
-            default:
-                System.out.println("Error");
-                break;
-        }
-
+        double num1 = Double.parseDouble(digitsStr[0]);
+        double num2 = Double.parseDouble(digitsStr[2]);
+        double result = calculateResult(num1, num2, digitsStr);
+        checkCorrectnessOrPrintResult(result);
     }
-    public static void correctnessOfResult(double result) {
-        if (Double.isNaN(result)){
+
+    public static void checkCorrectnessOrPrintResult(double result) {
+        if (Double.isNaN(result)) {
             System.out.println("Error. Not a Number");
-            return;
-        }
-        else {
+        } else {
             System.out.println(result);
-            return;
         }
     }
 
+    public static double calculateResult(double num1, double num2, String[] digitsStr) {
+        switch (digitsStr[1]) {
+            case ("+"):
+                return num1 + num2;
+            case ("-"):
+                return num1 - num2;
+            case ("/"):
+                return num1 / num2;
+            case ("*"):
+                return num1 * num2;
+            default:
+                System.out.println("Error. There is no such operation");
+                break;
+        }
+        return 0;
+    }
 }
